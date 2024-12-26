88bfb5e79c55c0ffd4170bde45907e76c7ce8c0e
devSurfer <2929408642@qq.com>
2024-12-26 23:32:17 +0800

    新功能(controller,xml): 整合spring-mvc框架 - 在web.xml中注册 dispatcherServlet，扫描 spring-mvc.xml 创建子容器（son ioc），对请求进行分发（找到对应处理器），并设置tomcat启动时一起加载 - 创建spring-mvc.xml   - 1.扫描controller处理器：<context:component-scan base-package="com.surfur.ssm.controller"/>   - 2.<mvc:annotation-driven/>            注意: 导入mvc命名空间!            mvc:annotation-driven 是一个整合标签                他会导入handlerMapping和handlerAdapter，还会导入json数据格式转化器等等   - 3.viewResolver 不需要配置,因为我们不需要查找逻辑视图!!!（前后端分离项目）   - 4.放行静态资源：<mvc:default-servlet-handler/>   - 5.配置动态页面语言jsp的视图解析器,快速查找jsp

---
+++ src/main/java/com/surfur/ssm/controller/EmpController.java
@@ -0,0 +1,31 @@
+package com.surfur.ssm.controller;
+
+import com.surfur.ssm.domain.EmpEntity;
+import com.surfur.ssm.service.EmpService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Dev Surfer
+ * @version 1.0.0
+ * date 2024/12/26 23:15
+ * description emp控制层处理器
+ */
+@RestController
+@RequestMapping("/emp")
+@AllArgsConstructor
+public class EmpController {
+
+    private final EmpService empService;
+
+    @GetMapping("{id}")
+    public ResponseEntity<EmpEntity> findEmpById(@PathVariable("id") Integer id) {
+        return new ResponseEntity<EmpEntity>(HttpStatus.OK);
+    }
+
+}
